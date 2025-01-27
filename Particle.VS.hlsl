a5810cf613a8abbacfb8c7767a912d52ba857b1f
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-01-27 15:39:29 +0900

    完了

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -4,24 +4,21 @@ struct Transformationmatrix
 {
     float32_t4x4 WVP;
     float32_t4x4 World;
-    //float32_t4 color;
 };
-ConstantBuffer<Transformationmatrix> gTransformationMatrix : register(b0);
+ConstantBuffer<Transformationmatrix> gTransformationMatrices : register(b0);
 
-struct VertexShaderInput //input
+struct VertexShaderInput
 {
     float32_t4 position : POSITION0;
     float32_t2 texcoord : TEXCOORD0;
-    float32_t3 normal:NORMAL0;
 };
 
-VertexShaderOutput main(VertexShaderInput input)
+VertexShaderOutput main(VertexShaderInput input, uint32_t instanceId : SV_InstanceID)
 {
     VertexShaderOutput output;
-    output.position = mul(input.position, gTransformationMatrix.WVP);
-    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
+    // ÉèÅ[ÉãÉhç¿ïWïœä∑Ç∆éÀâe
+    output.position = mul(input.position, gTransformationMatrices[instanceId].WVP);
     output.texcoord = input.texcoord;
-    output.worldPosition = mul(input.position, gTransformationMatrix.World).xyz;
-   
+    
     return output;
 };
