6d818d2c3b31fcf63981d6372cbdabc7d7b58d8f
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-01-27 15:48:36 +0900

    完了

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,70 +1,29 @@
 #include "object3d.hlsli"
 
+//03_00 p42
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+
+//02_01 p6
 struct Material
 {
     float32_t4 color;
-    int32_t enableLighting;
-    
-    float shininess;
 };
 
-struct DirectionalLight
-{
-    float32_t4 color; //ライトの色
-    float32_t3 direction; //ライトの向き
-    float intensity; //輝度
-};
-
+ConstantBuffer<Material> gMaterial : register(b0);
+//02_00 p16 //02_01_修正 p6
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
 };
 
-struct Camera
-{
-    float32_t3 worldPosition;
-};
-
-Texture2D<float32_t4> gTexture : register(t0);
-ConstantBuffer<Material> gMaterial : register(b0);
-ConstantBuffer<Camera> gCamera : register(b2);
-ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
-
-SamplerState gSampler : register(s0);
-
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
-    float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
- 
-    if (gMaterial.enableLighting != 0)
-    {
-        float NdotL = dot(normalize(input.normal), normalize(-gDirectionalLight.direction));
-        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-
-        float3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
-        float3 reflectLight = reflect(normalize(gDirectionalLight.direction), normalize(input.normal));
-
-        float RdotE = dot(reflectLight, toEye);
-        float specularPow = pow(saturate(RdotE), 70.0f); //shines値変更
-
-     // 拡散反射
-        float3 diffuse =
-     gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
-     // 鏡面反射
-        float3 specular =
-     gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
-     // 拡散反射+鏡面反射
-        output.color.rgb = diffuse + specular;
-     // アルファは今まで通り
-        output.color.a = gMaterial.color.a * textureColor.a;
-    }
-    else
-    {
-        output.color = gMaterial.color * textureColor;
-
-    }
- 
+    ////03_00 p43 p49
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    output.color = gMaterial.color * textureColor; //p43
     return output;
 }
